7446779ca61a65d763bd40899c51e2319f1719d5
Nachiket Pusalkar <nachiket87@gmail.com>
2020-06-02 17:33:04 -0400

    144A

---
+++ codeforces144A.cpp
@@ -0,0 +1,46 @@
+/* https://codeforces.com/problemset/problem/144/A */
+#include<iostream>
+#include<algorithm>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    int arr[n];
+    int max = 0, max_pos=0;
+    int min = 0, min_pos=0;
+    for(int i=0; i<n; i++)
+    {
+       cin >> arr[i];
+    }
+    max = arr[0];
+    min = arr[0];
+
+    for(int i=0; i<n; i++)
+    {
+        if(arr[i]>max)
+        {
+            max = arr[i];
+            max_pos = i;
+        }
+    }
+
+    for(int i=0; i<n; i++)
+    {
+        if(arr[i]<=min)
+        {
+            min = arr[i];
+            min_pos = i;
+        }
+    }
+    int max_steps = max_pos;
+    if(max_pos > min_pos)
+    {
+       min_pos++; 
+    }
+    int min_steps = n - min_pos-1;
+    cout << min_steps + max_steps;
+
+}
